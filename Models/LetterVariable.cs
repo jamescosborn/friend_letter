66faff27526723e8c96140f636c9580c4b1b9d6a
James Osborn <jamescarlosborn@gmail.com>
2017-10-11 16:44:05 -0700

    add Models, Views.

---
+++ Models/LetterVariable.cs
@@ -0,0 +1,17 @@
+namespace FriendLetter.Models
+{
+  public class LetterVariable
+  {
+    private string _recipient;
+
+    public string GetRecipient()
+    {
+      return _recipient;
+    }
+
+    public void SetRecipient(string newRecipient)
+    {
+      _recipient = newRecipient;
+    }
+  }
+}
