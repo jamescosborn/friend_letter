d4ee8c0a3d4c832780d2ef5f66ec592c5c22deae
James Osborn <jamescarlosborn@gmail.com>
2017-10-11 17:07:35 -0700

    add form

--- Controllers/HomeControllers.cs
+++ Controllers/HomeControllers.cs
@@ -21,5 +21,18 @@ namespace FriendLetter.Controllers
     {
       return View();
     }
+    [Route("/form")]
+    public ActionResult Form()
+    {
+      return View();
+    }
+    [Route("/greeting_card")]
+    public ActionResult GreetingCard()
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.SetRecipient(Request.Query["recipient"]);
+      myLetterVariable.SetSender(Request.Query["sender"]);
+      return View("Hello", myLetterVariable);
+    }
   }
 }
